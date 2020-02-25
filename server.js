db3d71da235f227b74176a064beb1118ad243357
coljadu <manishk.yadav01@gmail.com>
2020-02-25 15:15:28 -0500

    heroku config

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(__dirname + "/dist"));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
+
+const forceSSL = function() {
+  return function(req, res, next) {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      return res.redirect(["https://", req.get("Host"), req.url].join(""));
+    }
+    next();
+  };
+};
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+app.listen(process.env.PORT || 8080);
